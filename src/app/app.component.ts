ede4b85889cd8e4485d7d53e58a5fd08fda336e7
ExpandT <slepovd80@gmail.com>
2021-06-29 15:11:27 +0300

    Removed unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,37 +9,4 @@ import { FormControl } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-
-  input = new FormControl('');
-
-  allItems : string;
-
-  items : any = [];
-
-  localStorage : any = [];
-
-
-  constructor() {
-    this.allItems = (localStorage.getItem('allItems') !== null) ? JSON.parse(localStorage.getItem('allItems')!) : [];
-    this.localStorage = this.allItems;
-  }
-  
-
-  addItem() {
-    if(this.input.value == '') {
-        return
-    };
-   this.localStorage.unshift({
-      name: this.input.value,
-      done: false
-    })
-    this.input.setValue('');
-    localStorage.setItem('allItems', JSON.stringify(this.localStorage));
-    console.log(this.localStorage);
-  }
-  
-  deleteTask(index: number) {
-    this.localStorage.splice(index, 1);
-  }
-
 }
