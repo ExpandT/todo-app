40af6541310a0a36f0d66ce9e5b2b2548afe723c
ExpandT <slepovd80@gmail.com>
2021-06-29 10:44:05 +0300

    Added Interface

--- src/app/task-details/task-details.component.ts
+++ src/app/task-details/task-details.component.ts
@@ -1,27 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { from } from 'rxjs';
-import { map } from 'rxjs/operators';
+
+interface TodoData{
+  id: number,
+  name: string,
+  done: boolean
+}
 
 @Component({
   selector: 'todo-task-details',
   templateUrl: './task-details.component.html',
   styleUrls: ['./task-details.component.scss']
 })
+
 export class TaskDetailsComponent implements OnInit {
 
  id!: number;
- data!: { id: number, name: string, done: boolean }[];
+ data!: TodoData[];
  todoItem: { id: number, name: string, done: boolean } | undefined;
 
- 
-  constructor(public readonly route: ActivatedRoute) { 
+
+  constructor(public readonly route: ActivatedRoute) {
 
   }
   get name(): string{
     return this.todoItem?.name ?? "unknown" ;
   }
-  
+
   ngOnInit(): void {
     const idString = this.route.snapshot.paramMap.get('id');
 
