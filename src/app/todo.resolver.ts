0966460c958dd532036b90069c0c69c69902f52b
ExpandT <slepovd80@gmail.com>
2021-06-30 08:49:09 +0300

    refactor(resolver): change resolver type

--- src/app/todo.resolver.ts
+++ src/app/todo.resolver.ts
@@ -3,11 +3,17 @@ import { Resolve} from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+interface TodoData{
+  id: number,
+  name: string,
+  done: boolean
+}
+
 @Injectable({
   providedIn: 'root'
 })
-export class TodoResolver implements Resolve<{}[]> {
-  resolve(): Observable<any> {
+export class TodoResolver implements Resolve<TodoData[]> {
+  resolve(): Observable<TodoData[]> {
     const allItems = localStorage.getItem('allItems');
 
     if (allItems === null) {
