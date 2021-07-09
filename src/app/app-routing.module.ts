49ba40e942658889981fa72f6b0d427799e5523b
ExpandT <slepovd80@gmail.com>
2021-07-09 11:16:06 +0300

    refactor(todo-app): add redirect to home from empty route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {path: 'search', loadChildren: ()=> import('./search/search.module').then(m => m.SearchModule)},
   {path: 'todo', loadChildren: () => import('./todo/todo.module').then(m => m.TodoModule)},
   {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
   {path: '404', loadChildren: () => import('./error-page/error-page.module').then(m => m.ErrorPageModule)},
-  {path: '', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', redirectTo: '404', pathMatch: 'full'}
 ];
 
